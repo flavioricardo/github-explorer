9d4d8e3835e311f22255168b97146c3af3960a2c
Flávio Ricardo <flavioricardo@live.com>
2022-01-31 01:50:56 -0300

    Criando rota do App

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
-import * as React from "react";
 import { render } from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
 
 render(
   <BrowserRouter>
-    <App />
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
   </BrowserRouter>,
   document.getElementById("root")
 );
